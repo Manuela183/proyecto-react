7c4a0e56ceec84b63f21fddb2c52d061d4351fa4
Juan Giraldo <juandiegogbo@gmail.com>
2022-05-18 12:29:44 -0500

    Se agrego el componente CampoTexto

--- taller-componentes/src/Componentes_generales.jsx
+++ taller-componentes/src/Componentes_generales.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Tabla } from './Tabla'
-import { Boton } from './Boton'
+import { Boton } from './boton'
+import { CampoTexto } from './CampoTexto'
 
 export function Aplicacion() {
   return (
     <React.Fragment>
         <Tabla/>
+        <CampoTexto/>
         <Boton/>
     </React.Fragment>
   )
