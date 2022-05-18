f9be82c9666c350da1b89a79603664331935c3c8
Manuela183 <mamarulandaa@unal.edu.co>
2022-05-18 11:53:50 -0500

    Se agrega nuevo componente con funcionalidad de botón desde bootstrap

---
+++ taller-componentes/src/boton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Boton() {
+    return (
+        <button type="button" class="btn btn-primary btn-lg btn-block" onclick="reccion()">Clic Aquí</button>
+    )
+};
