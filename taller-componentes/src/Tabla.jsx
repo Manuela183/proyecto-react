2ce44d0c23ffaf52f421085152937205ba748bff
Juan Giraldo <juandiegogbo@gmail.com>
2022-05-18 12:29:03 -0500

    Se cambio la informacion

--- taller-componentes/src/Tabla.jsx
+++ taller-componentes/src/Tabla.jsx
@@ -5,29 +5,30 @@ export function Tabla() {
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">id</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Red Social</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td>Daniel</td>
+                    <td>Diaz</td>
+                    <td>Tinder</td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>Manuela</td>
+                    <td>Marulanda</td>
+                    <td>Instagram</td>
                 </tr>
                 <tr>
                     <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
+                    <td>Juan</td>
+                    <td>Giraldo</td>
+                    <td>Reddit</td>
                 </tr>
             </tbody>
         </table>
