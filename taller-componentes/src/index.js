f48334c383bcf0d493ba9c5a3e55d14db2c82169
Juan Giraldo <juandiegogbo@gmail.com>
2022-05-18 11:59:30 -0500

    Se agrega estructura basica

--- taller-componentes/src/index.js
+++ taller-componentes/src/index.js
@@ -1,1 +1,13 @@
+import React from "react";
+import ReactDOM  from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Aplicacion} from "./Componentes_generales";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Aplicacion/>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
