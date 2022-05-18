00b796391e222222c567bd2d6659a9420d446c84
Juan Giraldo <juandiegogbo@gmail.com>
2022-05-18 11:57:43 -0500

    Se agrega estructural inicial con los componentes de Boton y Tabla

---
+++ taller-componentes/src/Componentes_generales.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Tabla } from './Tabla'
+import { Boton } from './boton'
+
+export function Aplicacion() {
+  return (
+    <React.Fragment>
+        <Tabla/>
+        <Boton/>
+    </React.Fragment>
+  )
+}
